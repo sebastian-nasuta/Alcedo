8ec6d4f0c76ab919063ee6cf409b9aff413d7be0
Sebastian Nasuta <sebastian.nasuta@kotrak.com>
2024-11-27 23:32:44 +0100

    Initialize .NET MAUI project with multi-platform support

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Alcedo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
