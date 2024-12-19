f520b60a39e8f9b41f4aa400486898eb7ac6c0b6
Sebastian Nasuta <sebastian.nasuta@gmail.com>
2024-12-19 16:17:31 +0100

    Add Alcedo.Services namespace and refresh app theme on init
    Included Alcedo.Services namespace to enhance functionality.
    Added SettingsService.RefreshAppTheme() call in App constructor
    to ensure the application theme is refreshed upon startup.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace Alcedo
+﻿using Alcedo.Services;
+
+namespace Alcedo
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            SettingsService.RefreshAppTheme();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
