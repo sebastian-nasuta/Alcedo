c71ae5070393cf5fbf29b5452f5009a18b3b7699
Sebastian Nasuta <sebastian.nasuta@gmail.com>
2024-12-19 16:15:22 +0100

    Add SettingsService for managing app theme settings
    Introduce a new SettingsService class within the Alcedo.Services namespace. This class provides methods to manage the application's theme settings:
    - LoadUserAppTheme(): Retrieves the user's saved theme from preferences and converts it to an AppTheme enum.
    - SetAppTheme(string? themeName): Converts a theme name string to an AppTheme enum and sets the application theme.
    - SetAppTheme(AppTheme? appTheme, bool saveToPreferences = true): Sets the application theme and optionally saves it to preferences.
    - RefreshAppTheme(): Reloads the user's saved theme and applies it without saving it again.

---
+++ Services/SettingsService.cs
@@ -0,0 +1,28 @@
+﻿namespace Alcedo.Services;
+
+internal class SettingsService
+{
+    internal static AppTheme? LoadUserAppTheme()
+    {
+        var savedTheme = Preferences.Get("UserAppTheme", AppTheme.Unspecified.ToString());
+        Enum.TryParse(typeof(AppTheme), savedTheme, out var currentTheme);
+        return (AppTheme?)currentTheme;
+    }
+
+    internal static void SetAppTheme(string? themeName)
+    {
+        Enum.TryParse(typeof(AppTheme), themeName, out var appTheme);
+        SetAppTheme((AppTheme?)appTheme);
+    }
+
+    internal static void SetAppTheme(AppTheme? appTheme, bool saveToPreferences = true)
+    {
+        if (Application.Current is not null)
+            Application.Current.UserAppTheme = appTheme ?? AppTheme.Unspecified;
+
+        if (saveToPreferences)
+            Preferences.Set("UserAppTheme", appTheme.ToString());
+    }
+
+    internal static void RefreshAppTheme() => SetAppTheme(LoadUserAppTheme(), false);
+}
