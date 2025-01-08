d11ebd6b2979a8642710ee5c14d5b108f6cd882f
Sebastian Nasuta <sebastian.nasuta@gmail.com>
2025-01-08 14:09:16 +0100

    Implement GetTagsAsync and improve error handling
    - Made `OllamaImageTaggingService` a partial class.
    - Implemented `GetTagsAsync` to generate tags from a base64 image.
    - Enhanced `GetTagDescriptionAsync` with better JSON error handling.
    - Moved `OllamaResponse` to `OllamaResponse.cs` and made it public.
    - Added `BuildSystemMessage` for constructing tag generation requests.
    - Removed `NotImplementedException` from `GetTagsAsync`.
    - Improved exception handling and error messages throughout the code.

--- Services/ImageTaggingService/OllamaImageTaggingService.cs
+++ Services/ImageTaggingService/OllamaImageTaggingService.cs
@@ -1,10 +1,9 @@
 ﻿using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace Alcedo.Services.ImageTaggingService;
 
-internal class OllamaImageTaggingService : IImageTaggingService
+internal partial class OllamaImageTaggingService : IImageTaggingService
 {
     private static readonly string ollamaUrl = "http://192.168.0.81:11434";
     private static readonly string ollamaApiUrl = ollamaUrl + "/api";
@@ -24,11 +23,6 @@ internal class OllamaImageTaggingService : IImageTaggingService
             // Handle exceptions (e.g., log the error, show a message to the user)
             throw new Exception($"An error occurred while testing the Ollama connection: {ex.Message}");
         }
-    }
-
-    public Task<ILookup<string, string>> GetTagsAsync(string base64Image, string? customTag = null)
-    {
-        throw new NotImplementedException();
     }
 
     public async Task<string> GetTagDescriptionAsync(string tag)
@@ -76,9 +70,85 @@ internal class OllamaImageTaggingService : IImageTaggingService
 
             var responseContent = await reader.ReadToEndAsync();
 
-            var result = JsonSerializer.Deserialize<OllamaResponse>(responseContent);
+            var result = JsonSerializer.Deserialize<OllamaResponse>(responseContent)
+                ?? throw new Exception("An error occurred while deserializing the tag description. Prompt response: " + responseContent);
 
-            return result?.Message.Content ?? string.Empty;
+            return result.Message.Content;
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions (e.g., log the error, show a message to the user)
+            throw new Exception($"An error occurred while generating tag description: {ex.Message}");
+        }
+    }
+
+    public async Task<ILookup<string, string>> GetTagsAsync(string base64Image, string? customTag = null)
+    {
+        try
+        {
+            var tagsDictionary = new Dictionary<string, string[]>();
+            if (!string.IsNullOrWhiteSpace(customTag))
+            {
+                customTag = customTag.Trim();
+
+                tagsDictionary.Add(
+                    "inktober",
+                    [
+                        customTag,
+                        "inktober",
+                        "inktober" + customTag,
+                        "inktober" + DateTime.UtcNow.Year,
+                        "inktober" + DateTime.UtcNow.Year + customTag,
+                        "inktober52",
+                        "inktober52" + customTag,
+                        "ink",
+                        "drawing",
+                        "blackAndWhite"
+                    ]);
+            }
+
+            var systemMessage = BuildSystemMessage(tagsDictionary);
+
+            var userMessage = new
+            {
+                role = "user",
+                images = new[] { base64Image }
+            };
+
+            var requestBody = new
+            {
+                model = "llama3.2-vision",
+                messages = new[] { systemMessage, userMessage }
+            };
+
+            var serializedRequestBody = JsonSerializer.Serialize(requestBody);
+
+            using var client = new HttpClient();
+            var response = await client.PostAsync(
+                ollamaApiChatUrl,
+                new StringContent(serializedRequestBody, Encoding.UTF8, "application/json"));
+
+            response.EnsureSuccessStatusCode();
+
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            using var reader = new StreamReader(responseStream);
+
+            var responseContent = await reader.ReadToEndAsync();
+
+            var result = JsonSerializer.Deserialize<OllamaResponse>(responseContent)
+                ?? throw new Exception("An error occurred while deserializing the tags. responseContent: " + responseContent);
+
+            var deserializedTags = JsonSerializer.Deserialize<Dictionary<string, string[]>>(result.Message.Content)
+                ?? throw new Exception("An error occurred while deserializing the tags. result.Message.Content: " + result.Message.Content);
+
+            foreach (var kvp in deserializedTags)
+            {
+                tagsDictionary.Add(kvp.Key, kvp.Value);
+            }
+
+            return tagsDictionary
+                .SelectMany(kvp => kvp.Value, (kvp, tag) => new { kvp.Key, Tag = tag })
+                .ToLookup(x => x.Key, x => x.Tag);
         }
         catch (Exception ex)
         {
@@ -87,15 +157,71 @@ internal class OllamaImageTaggingService : IImageTaggingService
         }
     }
 
-    private class OllamaResponse
+    private static object BuildSystemMessage(Dictionary<string, string[]> tagsDictionary)
     {
-        [JsonPropertyName("message")]
-        public MessageModel Message { get; set; } = null!;
+        var sb = new StringBuilder();
 
-        public class MessageModel
+        sb.AppendLine("You are an assistant that generates tags for Instagram images sent to you in base64 format.");
+        sb.AppendLine("""
+            ~TASKS~
+            1. Generate 10 tags that describe only the illustration, ignoring the style or technique.
+            2. Generate 5 tags to describe the style.
+            3. Generate 5 tags to describe the technique.
+            """);
+        sb.AppendLine("""
+            ~RULES~
+            - return only tags grouped in arrays named "general", "style" and "technique" in json format
+            - don't add any prefix signs like '#'
+            - all tags should consist of letters and numbers, without special characters
+            - if a tag consists of multiple words, use camelCase
+            """);
+
+        if (tagsDictionary.Count != 0)
         {
-            [JsonPropertyName("content")]
-            public string Content { get; set; } = null!;
+            var existingTags = JsonSerializer.Serialize(tagsDictionary);
+            sb.AppendLine($"""
+                ~EXISTING TAGS~
+                The following tags are already present and should not be generated again:
+                {existingTags}
+                """);
         }
+
+        sb.AppendLine("""
+            ~EXAMPLE~
+            {
+              "general": [
+                "sunset",
+                "ocean",
+                "waves",
+                "beach",
+                "palmTrees",
+                "silhouette",
+                "sky",
+                "clouds",
+                "scenic",
+                "horizon"
+              ],
+              "style": [
+                "realistic",
+                "vibrant",
+                "colorful",
+                "detailed",
+                "naturalistic"
+              ],
+              "technique": [
+                "oilPainting",
+                "brushStrokes",
+                "layering",
+                "blending",
+                "impasto"
+              ]
+            }
+            """);
+
+        return new
+        {
+            role = "system",
+            content = sb.ToString()
+        };
     }
 }
---
+++ Services/ImageTaggingService/OllamaResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Alcedo.Services.ImageTaggingService;
+
+internal partial class OllamaImageTaggingService
+{
+    public class OllamaResponse
+    {
+        [JsonPropertyName("message")]
+        public MessageModel Message { get; set; } = null!;
+
+        public class MessageModel
+        {
+            [JsonPropertyName("content")]
+            public string Content { get; set; } = null!;
+        }
+    }
+}
