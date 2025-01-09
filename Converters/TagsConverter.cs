4791f09e869ce05dd6f0554c5373d89c85aa0c7e
Sebastian Nasuta <sebastian.nasuta@gmail.com>
2025-01-09 15:38:12 +0100

    Add TagsConverter class for string-list conversion
    Introduce TagsConverter in Alcedo.Converters namespace.
    Implements IValueConverter with Convert and ConvertBack methods.
    Convert joins list of tags into a single string with # prefix.
    ConvertBack splits string into list, removing # prefix.
    Import System.Globalization for CultureInfo support.

---
+++ Converters/TagsConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Alcedo.Converters;
+
+public class TagsConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is List<string> tags)
+        {
+            return string.Join(" ", tags.Select(tag => "#" + tag));
+        }
+        return string.Empty;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string tagString)
+        {
+            var tags = tagString.Split(' ')
+                                .Where(tag => !string.IsNullOrWhiteSpace(tag))
+                                .Select(tag => tag.TrimStart('#'))
+                                .ToList();
+            return tags;
+        }
+        return new List<string>();
+    }
+}
